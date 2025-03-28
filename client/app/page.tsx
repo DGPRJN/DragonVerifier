b41d1ec17612d600f478c70d2c1f42de6da4f2f2
Reid <reidcato5@gmail.com>
2025-03-28 13:44:01 -0500

    Added Location Getter

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,16 +1,38 @@
 //TODO Get rid of this, we should try to do as much rendering on the server as possible
 "use client";
 import {Box, Button, Typography, Container} from "@mui/material";
+import React, { useState } from 'react'
 
 const CheckinButton = () => {
-  const handleClick = () => {
-    console.log("Login button clicked");
-  }
-    return(
-      <Button variant="contained" color="primary" onClick={handleClick}>Check-in</Button>
+  const [location, setLocation] = useState<string | null>(null);
+  
+  const getLocation = () => {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          const lat = position.coords.latitude;
+          const lon = position.coords.longitude;
+          setLocation(`Latitude: ${lat}, Longitude: ${lon}`);
+          alert('Success!');
+        },
+        (error) => {
+          alert('Error getting location: ' + error.message);
+        }
+      );
+    } else {
+      alert('Geolocation is not supported by this browser.');
+    }
+  };
+
+
+  return(
+    <div>
+      <Button variant="contained" color="primary" onClick={getLocation}>Check-in</Button>
+      {location && <p>{location}</p>}
+    </div>
   );
 
-  };
+};
 
 export default function Home() {
   return (
@@ -25,3 +47,8 @@ export default function Home() {
     </>
   );
 }
+
+
+
+
+  
