5e652860fe5bcff33ee67224bb044b8377d074a1
Joseph McElderry <joewmcelderry@gmail.com>
2025-04-04 13:43:55 -0500

    Calendar now only displays months

--- client/app/(user_side)/Calendar/calender.tsx
+++ client/app/(user_side)/Calendar/calender.tsx
@@ -1,9 +1,10 @@
 "use client";
-//need to run npm install react-big-calendar moment
-import { Calendar, momentLocalizer } from "react-big-calendar";
+import { Calendar, momentLocalizer, View } from "react-big-calendar";
 import moment from "moment";
-//import 'react-big-calendar/lib/sass/styles';
-// https://github.com/jquense/react-big-calendar
+import "react-big-calendar/lib/css/react-big-calendar.css"; // https://github.com/jquense/react-big-calendar
+import MonthView from './customView.tsx'
+import dynamic from "next/dynamic";
+import { useState } from 'react';
 import "react-big-calendar/lib/css/react-big-calendar.css";
 
 //Apparently we are using moment?
@@ -11,7 +12,7 @@ const localizer = momentLocalizer(moment);
 
 const events = [
   {
-    title: 'My Event',
+    title: 'Some Class Event',
     start: new Date('2025-04-12T13:45:00-05:00'),
     end: new Date('2025-04-12T14:00:00-05:00')
   },
@@ -39,14 +40,25 @@ const eventStyleGetter = (event, start, end, isSelected) => {
 };
 
 const MyCalendar = () => {
+    const [view, setView] = useState<View>('month');
+    const [date, setDate] = useState(new Date());
   return <Calendar 
+      key={JSON.stringify(events)} //Supposedly a work around to make sure the buttons on the calendar wor
       localizer={localizer} 
       events={events} 
       startAccessor="start" 
       endAccessor="end" 
-      //style={ calendarStyle }
       style={calendarStyle}
-      eventPropGetter={eventStyleGetter} />;
+      eventPropGetter={eventStyleGetter} 
+      view={view}
+      onView={setView}
+      date={date}
+      onNavigate={setDate}
+      views={{ month: true }}
+      />;
 };
+
+// Export the calendar dynamically with SSR disabled
+//const MyCalendar = dynamic(() => Promise.resolve(MyCalendarInner), {ssr: false});
 
 export default MyCalendar;
