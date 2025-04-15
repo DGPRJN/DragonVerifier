65d85a9457cc8b4a0f2f71bd5b9685a246274c89
Joseph McElderry <joewmcelderry@gmail.com>
2025-04-15 15:16:22 -0500

    fixed header

--- client/app/components/NavBar.tsx
+++ client/app/components/NavBar.tsx
@@ -4,6 +4,7 @@ import React, { useState } from "react";
 import dynamic from "next/dynamic";
 import Link from "next/link";
 import {
+  Container,
   Drawer,
   Button,
   List,
@@ -14,67 +15,63 @@ import {
   Divider,
 } from "@mui/material";
 
-const header = () => {
-    return (
-    <>
-      <Container maxWidth="lg" sx={{ bgcolor: "lightgray", px: 4, pt: 1 }}>
+// Header component
+const Header = ({ toggleDrawer }: { toggleDrawer: (open: boolean) => () => void }) => {
+  return (
+    <Container maxWidth="lg" sx={{ bgcolor: "lightgray", px: 4, pt: 1 }}>
       <Button onClick={toggleDrawer(true)}>Open Menu</Button>
-        <Typography variant="body1" sx={{ textAlign: "center" }}>
-          Random info idk man
-        </Typography>
-      </Container>
-    </>
+      <Typography variant="body1" sx={{ textAlign: "center" }}>
+        Random info idk man
+      </Typography>
+    </Container>
   );
-}
+};
 
-// Prevents Next.js from rendering NavBar on the server
-const NavBar = dynamic(() => Promise.resolve(NavBarContent), { ssr: false });
-
+// Drawer + Header wrapper
 function NavBarContent() {
   const [open, setOpen] = useState(false);
-
   const toggleDrawer = (newOpen: boolean) => () => {
     setOpen(newOpen);
   };
 
   return (
     <>
+      <Header toggleDrawer={toggleDrawer} />
       <Drawer anchor="left" open={open} onClose={toggleDrawer(false)}>
-      <List>
-    {[
-      { text: "Home", href: "/" },
-      { text: "Calendar", href: "/Calendar" },
-      { text: "Courses", href: "/Classes" },
-    ].map(({ text, href }) => (
-      <ListItem key={`${href}-${text}`} disablePadding>
-        <Link href={href} passHref legacyBehavior>
-          <ListItemButton component="a" onClick ={toggleDrawer(false)}>
-            <Typography>{text}</Typography>
-          </ListItemButton>
-        </Link>
-      </ListItem>
-    ))}
-
-    <Divider />
-
-    <ListSubheader>More</ListSubheader>
-    {[
-      { text: "About Us", href: "/about" },
-      { text: "Contacts", href: "/contact_us" },
-      { text: "Login/Account", href: "/login" },
-    ].map(({ text, href }) => (
-      <ListItem key={href} disablePadding>
-        <Link href={href} passHref legacyBehavior>
-          <ListItemButton component="a" onClick={toggleDrawer(false)}>
-            <Typography>{text}</Typography>
-          </ListItemButton>
-        </Link>
-      </ListItem>
-    ))}
-  </List>
+        <List>
+          {[
+            { text: "Home", href: "/" },
+            { text: "Calendar", href: "/Calendar" },
+            { text: "Courses", href: "/Classes" },
+          ].map(({ text, href }) => (
+            <ListItem key={`${href}-${text}`} disablePadding>
+              <Link href={href} passHref legacyBehavior>
+                <ListItemButton component="a" onClick={toggleDrawer(false)}>
+                  <Typography>{text}</Typography>
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+          <Divider />
+          <ListSubheader>More</ListSubheader>
+          {[
+            { text: "About Us", href: "/about" },
+            { text: "Contacts", href: "/contact_us" },
+            { text: "Login/Account", href: "/login" },
+          ].map(({ text, href }) => (
+            <ListItem key={href} disablePadding>
+              <Link href={href} passHref legacyBehavior>
+                <ListItemButton component="a" onClick={toggleDrawer(false)}>
+                  <Typography>{text}</Typography>
+                </ListItemButton>
+              </Link>
+            </ListItem>
+          ))}
+        </List>
       </Drawer>
     </>
   );
 }
 
-export default NavBar;
+// Export with dynamic import if needed
+export default dynamic(() => Promise.resolve(NavBarContent), { ssr: false });
